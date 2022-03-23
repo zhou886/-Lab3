9e198fff9b594ffd51ae67024647b1101abe3b21
zyx <1272686158@qq.com>
2022-03-23 11:30:47 +0800

    fix bugs

--- bin/test.c
+++ bin/test.c
@@ -1,9 +1,1 @@
-#include <stdio.h>
-int main() {
-    printf("hello world");
-    int a=123u,b=0214,c=0x12345u;
-    for (int i=0;i<a;i++) {
-        printf("%d %d\n",b+c-i);
-    }
-    return 0;
-}
+int sum(int a, int b) {return a + b; }
